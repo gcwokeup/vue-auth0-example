a8317cb7f683ded6eaf72b938deaefe5412ab1af
Gino Paul Mendoza Capio <ginocapio@gmail.com>
2020-08-07 11:24:46 -0500

    Add router and views

---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <div class="home">
+      <img alt="Vue logo" src="../assets/logo.png" />
+
+      <!-- Check that the SDK client is not currently loading before accessing is methods -->
+      <div v-if="!$auth.loading">
+        <!-- show login when not authenticated -->
+        <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+        <!-- show logout when authenticated -->
+        <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+      </div>
+    </div>
+    <GiphySearch msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import GiphySearch from "@/components/GiphySearch";
+export default {
+  name: "home",
+  components: {
+    GiphySearch
+  },
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
+};
+</script>
