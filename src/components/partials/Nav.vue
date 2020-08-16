baf86ab4b4ce56f3e1fd5999741d87f14a699e67
Gino Paul Mendoza Capio <ginocapio@gmail.com>
2020-08-16 15:30:14 -0500

    Add template wrapper element

--- src/components/partials/Nav.vue
+++ src/components/partials/Nav.vue
@@ -1,16 +1,18 @@
-<div class="navbar-end">
-  <div class="navbar-item">
-    <div class="buttons">
-      <!-- Check that the SDK client is not currently loading before accessing is methods -->
-      <div v-if="!$auth.loading">
-        <!-- show login when not authenticated -->
-        <a v-if="!$auth.isAuthenticated" @click="login" class="button is-dark"><strong>Sign in</strong></a>
-        <!-- show logout when authenticated -->
-        <a v-if="$auth.isAuthenticated" @click="logout" class="button is-dark"><strong>Log out</strong></a>
+<template>
+  <div class="navbar-end">
+    <div class="navbar-item">
+      <div class="buttons">
+        <!-- Check that the SDK client is not currently loading before accessing is methods -->
+        <div v-if="!$auth.loading">
+          <!-- show login when not authenticated -->
+          <a v-if="!$auth.isAuthenticated" @click="login" class="button is-dark"><strong>Sign in</strong></a>
+          <!-- show logout when authenticated -->
+          <a v-if="$auth.isAuthenticated" @click="logout" class="button is-dark"><strong>Log out</strong></a>
+        </div>
       </div>
     </div>
   </div>
-</div>
+</template>
 <script>
 export default {
   name: 'Nav',
